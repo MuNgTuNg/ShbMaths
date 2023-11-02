bfeb651e8c06f2451822c56ce3567e6f2cebd308
Shay Howard-Bennett <shamus360@googlemail.com>
2023-11-02 14:20:12 +0000

    initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+CYAN='\033[0;36m'
+echo -e "${CYAN} Building project :)"
+
+cd build && make
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+CYAN='\033[0;36m'
+echo -e "${CYAN} Setting up project :)"
+
+cmake -S . -B build
